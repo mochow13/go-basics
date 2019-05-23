256aff28765a7a35eec8ea4d555f879380e66ad6
mochow13 <md.mottakin.chowdhury@gmail.com>
2019-05-24 03:03:46 +0600

    a way to solve the previous problem

--- 15_goroutines/main.go
+++ 15_goroutines/main.go
@@ -7,13 +7,10 @@ import (
 
 func main() {
 	var name = "Walter White"
-	/* anonymous function, invoking immediately
-	the function won't print Walter White because before
-	it gets a chance, the name is reassigned, so it prints
-	Willy Wonka */
-	go func() {
-		fmt.Println(name)
-	}()
+	// pass name before it gets changed, a way to solve the problem
+	go func(n string) {
+		fmt.Println(n)
+	}(name)
 	name = "Willy Wonka"
 	/* sleep is still necessary because main executes before even
 	the anonymous function has a scope to print the name */
