bc9131a5fbf4c7d59d3db97197ad879b382f9c44
mochow13 <md.mottakin.chowdhury@gmail.com>
2019-05-24 03:10:01 +0600

    simple waitgroup example

--- 15_goroutines/main.go
+++ 15_goroutines/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
+
+// syncs multiple goroutines
+var wg = sync.WaitGroup{}
 
 func main() {
 	var name = "Walter White"
-	// pass name before it gets changed, a way to solve the problem
+	wg.Add(1) // tell waitgroup we want to sync this goroutine
 	go func(n string) {
 		fmt.Println(n)
+		wg.Done() // this goroutine has done its execution
 	}(name)
 	name = "Willy Wonka"
-	/* sleep is still necessary because main executes before even
-	the anonymous function has a scope to print the name */
-	time.Sleep(100 * time.Millisecond)
+	wg.Wait() // no sleep needed, just wait for the goroutines
+	// we print Walter White, as we want
 }
