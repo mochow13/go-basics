f7aa4a1364316c0c1bf086bef2d2d3d2395b8453
mochow13 <md.mottakin.chowdhury@gmail.com>
2019-05-24 16:04:12 +0600

    mutex example

--- 15_goroutines/main.go
+++ 15_goroutines/main.go
@@ -5,29 +5,33 @@ import (
 	"sync"
 )
 
+// concurrency and parallelism is destroyed in this example
+// it is like a sequential execution, but we show this as an example of mutex
 var wg = sync.WaitGroup{}
 var counter = 0
+var m = sync.RWMutex{}
 
 func main() {
 	for i := 0; i < 10; i++ {
 		wg.Add(2)
+		// Lock on goroutine, not inside the goCount(), but outside
+		// doing a read lock
+		m.RLock()
 		go goCount()
+		m.Lock()
 		go incCount()
 	}
 	wg.Wait()
 }
 
-/* counter will be printed in a very chaotic way and different
-in each run. This is because the goroutines are racing against each
-other to complete execution and there is no synchronization among them
-*/
-
 func goCount() {
 	fmt.Println("Count is now ", counter)
+	m.RUnlock() // unlock when we are done with operation
 	wg.Done()
 }
 
 func incCount() {
 	counter++
+	m.Unlock()
 	wg.Done()
 }
